16b9ee5180116986993e98259bdb17ddc404d6c2
girish <girish.sk@gmail.com>
2017-07-09 02:07:53 -0700

    Unscented Kalman filter

--- CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -366,6 +366,23 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
     P_ = (I - K * H_laser_) * P_;
 
 
+
+    VectorXd diff = VectorXd(2);
+    diff = z_pred.array() - z.array();
+    while(diff[1] > PI)
+    {
+        diff[1] = diff[1] - 2*PI;
+    }
+    while(diff[1] < -1*PI)
+    {
+        diff[1] = diff[1] + 2*PI;
+    }
+
+    double nis = diff.transpose()*S.inverse()*diff;
+
+    cout<<"NIS"<<nis<<endl;
+
+
 }
 
 
@@ -379,13 +396,6 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
     You'll also need to calculate the radar NIS.
     */
     int n_z = 3;
-//    Xsig_pred_ <<
-//              5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
-//            1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
-//            2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
-//            0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
-//            0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
-
 
     MatrixXd S = MatrixXd(n_z,n_z);
     S.fill(0);
@@ -457,9 +467,7 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
         diff[1] = diff[1] + 2*PI;
     }
 
-    cout<<"-------DIFF in radar---------"<<endl;
-    cout<<diff<<endl;
-    cout<<"----------------"<<endl;
+
     double nis = diff.transpose()*S.inverse()*diff;
 
     cout<<"NIS"<<nis<<endl;
